778569cf7e58539c7f170fd71aa278d4f6827280
Maxim_Boiko <Maksimboiko222@gmail.com>
2024-08-06 12:09:50 +0300

    react hw 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,47 @@
-const App = () => {};
+import { useState, useEffect } from 'react';
+import ContactForm from './components/ContactForm/ContactForm';
+import ContactList from './components/ContactList/ContactList';
+import SearchBox from './components/SearchBox/SearchBox';
+import css from './App.module.css';
+import { nanoid } from 'nanoid';
+
+const App = () => {
+  const data = localStorage.getItem('contacts');
+  const [contacts, setContacts] = useState(JSON.parse(data));
+  const [filterValue, setFilterValue] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const onSearchContact = event => {
+    const value = event.target.value;
+    setFilterValue(value);
+  };
+
+  const filteredContacts = contacts.filter(contact => {
+    return contact.name.toLowerCase().includes(filterValue.toLowerCase());
+  });
+
+  const onAddContact = values => {
+    const finalContact = { ...values, id: nanoid() };
+    setContacts([finalContact, ...contacts]);
+  };
+
+  const onDeleteContact = id => {
+    setContacts(contacts.filter(contact => contact.id !== id));
+  };
+
+  return (
+    <div className={css.appContainer}>
+      <h1>Phonebook</h1>
+      <ContactForm onAddContact={onAddContact} />
+      <SearchBox onSearchContact={onSearchContact} value={filterValue} />
+      <ContactList
+        contacts={filteredContacts}
+        onDeleteContact={onDeleteContact}
+      />
+    </div>
+  );
+};
 export default App;
