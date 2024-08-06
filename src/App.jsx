5d2909b8cf7cfdb9a49346ace8706712e8a26602
Maxim_Boiko <Maksimboiko222@gmail.com>
2024-08-06 12:15:24 +0300

    react hw 3 update

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,14 @@ import { nanoid } from 'nanoid';
 
 const App = () => {
   const data = localStorage.getItem('contacts');
-  const [contacts, setContacts] = useState(JSON.parse(data));
+  const [contacts, setContacts] = useState(
+    JSON.parse(data) || [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ]
+  );
   const [filterValue, setFilterValue] = useState('');
 
   useEffect(() => {
